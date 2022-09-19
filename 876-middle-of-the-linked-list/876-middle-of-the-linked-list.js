798cba8b3793f6fafb94bce35beed55a533cda47
Johnwav <102820657+johnwav@users.noreply.github.com>
2022-09-20 00:49:50 +0100

    Time: 84 ms (60.46%), Space: 42.1 MB (51.82%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -1,8 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 var middleNode = function(head) {
-    let fast = slow = head;
-    while (fast && fast.next) {
-        fast = fast.next.next;
-        slow = slow.next;
+    
+   let fastPointer = slowPointer = head
+    
+    while (fastPointer && fastPointer.next) {
+        
+        fastPointer = fastPointer.next.next
+        slowPointer = slowPointer.next
+        
     }
-    return slow;
+    return slowPointer
+    
 };
